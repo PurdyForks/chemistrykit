bf563269351d7d129d8e09f69034108957111202
Joe Purdy <joe@poweredbypurdy.com>
2015-12-12 00:06:40 -0800

    updated base formula

--- lib/templates/chemistrykit/formulas/lib/formula.rb
+++ lib/templates/chemistrykit/formulas/lib/formula.rb
@@ -4,4 +4,50 @@
 # the rest of your formulas
 class Formula < ChemistryKit::Formula::Base
 
+	attr_reader :driver
+
+	def initialize(driver)
+    @driver = driver
+    driver.manage.window.resize_to(1260, 1190)
+  end
+
+  def visit(url='/')
+    driver.get(ENV['BASE_URL'] + url)
+  end
+
+  def find(locator)
+    driver.find_element locator
+  end
+
+  def clear(locator)
+    find(locator).clear
+  end
+
+  def type(locator, input)
+    find(locator).send_keys input
+  end
+
+  def click_on(locator)
+    find(locator).click
+  end
+
+  def displayed?(locator)
+    driver.find_element(locator).displayed?
+    true
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+      false
+  end
+
+  def text_of(locator)
+    find(locator).text
+  end
+
+  def title
+    driver.title
+  end
+
+  def wait_for(seconds=5)
+    Selenium::WebDriver::Wait.new(:timeout => seconds).until { yield }
+  end
+
 end
